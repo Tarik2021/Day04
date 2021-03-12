c19626f47e33907565a9d2eb3324273eae33b62d
Tarik2021 <tariks2021@gmail.com>
2021-03-12 08:05:38 +0100

    Day04 exercises

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    
+    var myFirstName = "Tarik";
+    var myLastName = "Sejtanic";
+    var myHomeTown = "Sarajevo";
+
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+}
+}
+console.log(main());
+module.exports = main;
